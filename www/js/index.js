62f075e5afc35057bc44fe8c01c313a5aac634de
han-bbo <78396716+han-bbo@users.noreply.github.com>
2022-05-09 14:33:22 +0900

    Sprite Follow Mouse Pointer実装”

--- www/js/index.js
+++ www/js/index.js
@@ -25,6 +25,7 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const PIXI = __importStar(require("pixi.js"));
 let app;
+let player;
 window.onload = () => {
     app = new PIXI.Application({
         width: window.innerWidth,
@@ -32,4 +33,21 @@ window.onload = () => {
         backgroundColor: 0xAAAAAA
     });
     document.body.appendChild(app.view);
+    // player object
+    player = PIXI.Sprite.from("images/player.png");
+    player.anchor.set(0.5);
+    player.x = app.view.width / 2;
+    player.y = app.view.height / 2;
+    app.stage.addChild(player);
+    app.stage.interactive = true;
+    let onPointerDown = (e) => {
+        let pos = e.data.global;
+        player.x = pos.x;
+        player.y = pos.y;
+    };
+    // touch interaction
+    app.stage.interactive = true;
+    app.stage
+        .on("pointerdown", onPointerDown)
+        .on("pointermove", onPointerDown);
 };
--- www/ts/index.ts
+++ www/ts/index.ts
@@ -1,6 +1,7 @@
 import * as PIXI from "pixi.js"
 
 let app;
+let player: PIXI.Sprite;
 
 window.onload = () => {
     app = new PIXI.Application(
@@ -12,4 +13,27 @@ window.onload = () => {
     );
 
     document.body.appendChild(app.view)
+
+    // player object
+    player = PIXI.Sprite.from("images/player.png");
+    player.anchor.set(0.5);
+    player.x = app.view.width / 2;
+    player.y = app.view.height / 2;
+
+    app.stage.addChild(player)
+    app.stage.interactive = true;
+
+    let onPointerDown = (e: { data: { global: any; }; }) => {
+        let pos = e.data.global;
+
+        player.x = pos.x;
+        player.y = pos.y
+    }
+    
+
+    // touch interaction
+    app.stage.interactive = true;
+    app.stage
+    .on("pointerdown", onPointerDown)
+    .on("pointermove", onPointerDown)
 }
