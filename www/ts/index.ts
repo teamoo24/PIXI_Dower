38481f264404a9c685ff37624ff53e75f1d8d94e
han-bbo <78396716+han-bbo@users.noreply.github.com>
2022-05-09 11:58:49 +0900

    done basic project setup

--- www/ts/index.ts
+++ www/ts/index.ts
@@ -1,1 +1,15 @@
-console.log("is running")
+import * as PIXI from "pixi.js"
+
+let app;
+
+window.onload = () => {
+    app = new PIXI.Application(
+        {
+            width:window.innerWidth,
+            height:window.innerHeight,
+            backgroundColor: 0xAAAAAA
+        }
+    );
+
+    document.body.appendChild(app.view)
+}
